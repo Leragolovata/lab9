250daf1ffe7f17822f961f50b7582f9de35a90a4
USER <USER@DESKTOP-K0V9S8M>
2025-11-03 12:02:52 +0200

    Add project files.

---
+++ lab9/lab9.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    double num;
+    int countPositive = 0;
+
+    for (int i = 1; i <= 10; i++) {
+        cout << "Введіть число " << i << ": ";
+        cin >> num;
+
+        if (num > 0) {
+            countPositive++;
+        }
+    }
+
+    cout << "Кількість додатних чисел: " << countPositive << endl;
+
+    return 0;
+}
